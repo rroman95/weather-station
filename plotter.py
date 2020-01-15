4683275a4bc32049341f2d268a8b15ed4e81027d
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-01-15 23:20:27 +0100

    A plotter mostantol CSV filebol olvas

--- plotter.py
+++ plotter.py
@@ -7,7 +7,7 @@ import bme280_output
 
 plt.style.use('fivethirtyeight')
 
-x_vals = []
+x = []
 y1 = []
 y2 = []
 
@@ -16,16 +16,18 @@ humidity, pressure, ambient_temperature = bme280_output.read_all()
 index = count()
 
 def animate(i):
-    humidity, pressure, ambient_temperature = bme280_output.read_all()
-    x_vals.append(next(index))
-    y1.append(ambient_temperature)
-    y2.append(humidity)
+    data = pd.read_csv('data.csv')
+    x = data['Time']
+    y1 = data['Temperature']
+    y2 = data['Humidity']
     plt.cla()
-    plt.plot(x_vals,y1, label = 'Temperature')
-    plt.plot(x_vals,y2, label = 'Humidity')
+    plt.plot(x,y1, label = 'Temperature')
+    plt.plot(x,y2, label = 'Humidity')
     plt.legend (loc='upper left')
+    plt.tight_layout()
 
 ani = FuncAnimation(plt.gcf(), animate, interval=100)
 
 plt.tight_layout()
 plt.show()
+
