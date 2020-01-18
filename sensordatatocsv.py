9ebc21e8c88c730c572303adfe976179a5114c02
rroman95 <44027537+rroman95@users.noreply.github.com>
2020-01-18 15:14:05 +0100

    atlagos ertekek mentese, nyomas (Bar) hozzaadva

--- sensordatatocsv.py
+++ sensordatatocsv.py
@@ -1,38 +1,83 @@
+#!/bin/env python3
+import csv
 import random
-from itertools import count
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import bme280_output
+import time
 import os.path
+import bme280_output
+from datetime import datetime
 from datetime import date
+import datetime
 
-plt.style.use('fivethirtyeight')
-
-x = []
-y1 = []
-y2 = []
-
-humidity, pressure, ambient_temperature = bme280_output.read_all()
-
-index = count()
 dir = r"/home/pi/weather-station/data/"
-filename = str(date.today())
+fieldnames = ["Time", "Temperature", "Humidity","Pressure",
+              "Avg.Temp","Avg.Humidity","Avg.Pressure","Min.Temp","Max.Temp"]
+while True:
+    
+    
+    Time = 0
+    Temperature= 0
+    Humidity = 0
+    Bar = 0
+    all_temperature = 0
+    all_humidity = 0
+    all_bar = 0
+    avg_temperature = 0
+    avg_humidity = 0
+    avg_bar = 0
+    max_temp = 0
+    min_temp = 0
+    NumberOfIterations = 1
+    filename = str(date.today())
+    print ("reset timer")
+    humidity, pressure, ambient_temperature = bme280_output.read_all()
+    min_temp = ambient_temperature
+    max_temp = ambient_temperature
+    print ("tmp:",ambient_temperature)    
+    with open(os.path.join(dir,filename+'.csv'), 'w') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
 
-def animate(i):
-    data = pd.read_csv(os.path.join(dir,filename+'.csv'))
-    x = data['Time']
-    y1 = data['Temperature']
-    y2 = data['Humidity']
-    plt.cla()
-    plt.plot(x,y1, label = 'Temperature')
-    plt.plot(x,y2, label = 'Humidity')
-    plt.xticks(rotation=90)
-    plt.legend (loc='upper left')
-    plt.tight_layout()
+    now = datetime.datetime.now()
+    endtime = now.replace(hour = 23, minute = 59, second = 0, microsecond= 0)
 
-ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+    while (now < endtime):
 
-plt.tight_layout()
-plt.show()
+        with open(os.path.join(dir,filename+'.csv'), 'a') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            humidity, pressure, ambient_temperature = bme280_output.read_all()
+            current_time = now.strftime("%H:%M:%S")
+            Bar = pressure / 1000
+            
+            if (ambient_temperature > max_temp):
+                max_temp = ambient_temperature
+            if (ambient_temperature < min_temp):
+                min_temp = ambient_temperature
+            
+            all_temperature += ambient_temperature
+            all_humidity += humidity
+            all_bar += Bar
+            
+            avg_temperature = all_temperature / NumberOfIterations
+            avg_humidity = all_humidity / NumberOfIterations
+            avg_bar = all_bar / NumberOfIterations
+            
+            info = {
+                "Time": current_time,
+                "Temperature": round(ambient_temperature,2),
+                "Humidity": round(humidity,2),
+                "Pressure": round(Bar,2),
+                "Avg.Temp": round(avg_temperature,2),
+                "Avg.Humidity": round(avg_humidity,2),
+                "Avg.Pressure": round(avg_bar, 2),
+                "Min.Temp": round(min_temp, 2),
+                "Max.Temp": round (max_temp, 2)                
+            }
 
+            csv_writer.writerow(info)
+            print(current_time,round(ambient_temperature,2), round(humidity,2),round (Bar,2))
+
+            
+            time.sleep(1)
+            NumberOfIterations += 1
+            now = datetime.datetime.now()   
+    time.sleep(90)
